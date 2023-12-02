7298a76b435e842d0cf30496b562f5ae696a2e6d
Jonatan Missora <jonatanjmissora@gmail.com>
2023-12-02 16:44:10 -0300

    02-12

---
+++ src/constant.js
@@ -0,0 +1,85 @@
+import foto1 from "./assets/foto1.jpg"
+import foto2 from "./assets/foto2.jpg"
+import foto3 from "./assets/foto3.jpg"
+import foto4 from "./assets/foto4.jpg"
+import foto5 from "./assets/foto5.jpg"
+import foto6 from "./assets/foto6.jpg"
+import foto7 from "./assets/foto7.jpg"
+import foto8 from "./assets/foto8.jpg"
+import foto9 from "./assets/foto9.jpg"
+import foto10 from "./assets/foto10.jpg"
+import foto11 from "./assets/foto11.jpg"
+import foto12 from "./assets/foto12.jpg"
+import foto13 from "./assets/foto13.jpg"
+
+const fotos =
+{
+  "01": {
+    src: foto1,
+    alt: "centro de mesa y copas",
+    title: "centro_de_mesa_y_copas"
+  },
+  "02": {
+    src: foto2,
+    alt: "novio con ramo",
+    title: "novio_con_ramo"
+  },
+  "03": {
+    src: foto3,
+    alt: "casamiento manos con anillos",
+    title: "casamiento_manos_con_anillos"
+  },
+  "04": {
+    src: foto4,
+    alt: "patio de punto alem",
+    title: "patio_de_punto_alem"
+  },
+  "05": {
+    src: foto5,
+    alt: "entrada de la calle punto alem",
+    title: "entrada_de_la_calle_punto_alem"
+  },
+  "06": {
+    src: foto6,
+    alt: "grupo de fiesta saludando",
+    title: "grupo_de_fiesta_saludando"
+  },
+  "07": {
+    src: foto7,
+    alt: "casamiento manos entrelazadas",
+    title: "casamiento_manos_entrelazadas"
+  },
+  "08": {
+    src: foto8,
+    alt: "carreta de fuego patio",
+    title: "carreta_de_fuego_patio"
+  },
+  "09": {
+    src: foto9,
+    alt: "mesa preparada para el evento",
+    title: "mesa_preparada_para_el_evento"
+  },
+  "10": {
+    src: foto10,
+    alt: "living 1 para recepcion",
+    title: "living_1_para_recepcion"
+  },
+  "11": {
+    src: foto11,
+    alt: "living 2 para recepcion",
+    title: "living_2_para_recepcion"
+  },
+  "12": {
+    src: foto12,
+    alt: "plato gourmet 1",
+    title: "plato_gourmet_1"
+  },
+  "13": {
+    src: foto13,
+    alt: "plato gourmet 2",
+    title: "plato_gourmet_2"
+  },
+
+}
+
+export default fotos
