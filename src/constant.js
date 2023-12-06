254ef94a14d1d740f20e5bdbf80b8bda7fa8f55a
Jonatan Missora <jonatanjmissora@gmail.com>
2023-12-06 17:06:35 -0300

    06-12

--- src/constant.js
+++ src/constant.js
@@ -21,33 +21,33 @@ const fotos =
   },
   "02": {
     src: foto2,
-    alt: "novio con ramo",
-    title: "novio_con_ramo"
+    alt: "anillos de boda",
+    title: "anillos_de_boda"
   },
   "03": {
     src: foto3,
-    alt: "casamiento manos con anillos",
-    title: "casamiento_manos_con_anillos"
+    alt: "novio arrojando ramo de bodas",
+    title: "novio_arrojando_ramo_de_bodas"
   },
   "04": {
     src: foto4,
-    alt: "patio de punto alem",
-    title: "patio_de_punto_alem"
+    alt: "pista de baile",
+    title: "pista_de_baile"
   },
   "05": {
     src: foto5,
-    alt: "entrada de la calle punto alem",
-    title: "entrada_de_la_calle_punto_alem"
+    alt: "recepcion 1 del salón",
+    title: "recepcion_1_del_salón"
   },
   "06": {
     src: foto6,
-    alt: "grupo de fiesta saludando",
-    title: "grupo_de_fiesta_saludando"
+    alt: "carretón de fuego",
+    title: "carretón_de_fuego"
   },
   "07": {
     src: foto7,
-    alt: "casamiento manos entrelazadas",
-    title: "casamiento_manos_entrelazadas"
+    alt: "plato gourmet 1",
+    title: "plato_gourmet_1"
   },
   "08": {
     src: foto8,
@@ -56,28 +56,28 @@ const fotos =
   },
   "09": {
     src: foto9,
-    alt: "mesa preparada para el evento",
-    title: "mesa_preparada_para_el_evento"
+    alt: "plato gourmet 2",
+    title: "plato_gourmet_2"
   },
   "10": {
     src: foto10,
-    alt: "living 1 para recepcion",
-    title: "living_1_para_recepcion"
+    alt: "plato gourmet 3",
+    title: "plato_gourmet_3"
   },
   "11": {
     src: foto11,
-    alt: "living 2 para recepcion",
-    title: "living_2_para_recepcion"
+    alt: "chefs emplatando",
+    title: "chefs_emplatando"
   },
   "12": {
     src: foto12,
-    alt: "plato gourmet 1",
-    title: "plato_gourmet_1"
+    alt: "recepcion 2 del salón",
+    title: "recepcion_2_del_salón"
   },
   "13": {
     src: foto13,
-    alt: "plato gourmet 2",
-    title: "plato_gourmet_2"
+    alt: "patio del salón",
+    title: "patio_del_salón"
   },
 
 }
